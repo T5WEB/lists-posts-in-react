73a3795cb0a759e86b55886dbe4beb44f9d68fa2
Tiago da Silva Lourenco <tiagomutum@gmail.com>
2021-06-07 23:11:41 -0300

    project finalized first part

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export const PostCard = ({ title, body, cover, id }) => {
+  return (
+    <div className="post">
+      <img src={cover} alt={title} />
+      <div className="post-content">
+        <h2>
+          {title} {id}
+        </h2>
+        <p>{body}</p>
+      </div>
+    </div>
+  );
+};
